c31655b52374e6452f78f554fb521c1bf0a3e829
itt2023117-byte <itt2023117@tec.rjt.ac.lk>
2025-11-10 05:21:46 +0530

    pathuu

--- frontend/src/pages/KnownGalaxiesPage.jsx
+++ frontend/src/pages/KnownGalaxiesPage.jsx
@@ -530,7 +530,7 @@ export default function KnownGalaxiesPage() {
 						className="bg-green-500 px-6 py-2 rounded font-bold text-white hover:bg-green-600"
 						onClick={() => setShowQuiz(true)}
 					>
-						Start Quiz
+						🌀 Start Quiz
 					</button>
 				</div>
 				{showQuiz && (
